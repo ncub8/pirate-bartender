d1e695f0581ba3f9c9af9238c2ab63593fdc9d76
John Moore <john@ncub8.com>
2015-01-28 20:21:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Arrrrrrr
---
+++ pirate_bartender.py
@@ -0,0 +1,40 @@
+import random
+
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?"
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola", "splash of simple syrup"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top", "twist of lemon"]
+}
+
+def askQuestions():
+  """Ask a user a series of questions about taste preferences"""
+  choices = {}
+  for key, value in questions.items():
+    answer = raw_input(value)
+    if (answer == "y") or (answer == "yes"):
+      choices[key] = True
+    else:
+      choices[key] = False
+  return choices
+    
+
+def mix(choices=askQuestions()):
+  """Construct drink based on taste preferences"""
+  drink = []
+  for key, value in ingredients.items():
+    if choices[key]:
+      drink.append(random.choice(value))
+  return drink
+  
+if __name__ == '__main__':
+   print(mix())
